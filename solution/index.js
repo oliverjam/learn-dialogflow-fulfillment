43222e814a3561d2cbc7173bec3601696305eeb2
oliverjam <oliverjamesphillips@gmail.com>
2020-06-28 17:43:16 +0100

    Port env var needs to be capitalised

--- solution/index.js
+++ solution/index.js
@@ -1,5 +1,5 @@
 const server = require("./server.js");
-const PORT = process.env.port || 3333;
+const PORT = process.env.PORT || 3333;
 
 const listener = server.listen(PORT, () =>
   console.log(`Listening on http://localhost:${listener.address().port}`)
