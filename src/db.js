417800c99ed69abd00ac30db7eba88bdbf07f79b
oliverjam <oliverjamesphillips@gmail.com>
2020-06-28 17:19:04 +0100

    Remove default db data

--- src/db.js
+++ src/db.js
@@ -2,18 +2,12 @@ const lowdb = require("lowdb");
 const Memory = require("lowdb/adapters/Memory");
 const FileSync = require("lowdb/adapters/FileSync");
 
-const defaultValue = {
-  appointments: [
-    { customer: "oliver", datetime: "2020-06-29T05:00:00+01:00" },
-    { customer: "sam", datetime: "2020-06-31T09:00:00+01:00" },
-  ],
-};
-// lowdb will create our json file populated with the default value
 const adapter =
   process.env.NODE_ENV === "test"
     ? new Memory()
-    : new FileSync(".data/db.json", { defaultValue });
+    : new FileSync(".data/db.json");
 
 const db = lowdb(adapter);
+db.defaults({ appointments: [] }).write();
 
 module.exports = db;
